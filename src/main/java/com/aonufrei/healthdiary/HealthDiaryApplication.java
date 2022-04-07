db88fad8d9eb88e59d267f646c171e0dc0b6217f
Artem Onufrei <onufrei.artem@gmail.com>
2022-04-08 02:18:08 +0300

    Added openapi info

--- src/main/java/com/aonufrei/healthdiary/HealthDiaryApplication.java
+++ src/main/java/com/aonufrei/healthdiary/HealthDiaryApplication.java
@@ -1,9 +1,12 @@
 package com.aonufrei.healthdiary;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Health Diary API", version = "0.1", description = "This application allows people to keep track of food and calories they consume to stay healthy"))
 public class HealthDiaryApplication {
 
 	public static void main(String[] args) {
